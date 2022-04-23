8a427dd5b5e0b5d557fa693b41beec675933c910
milos.garunovic <milos.garunovic@gmail.com>
2022-04-23 12:50:03 +0200

    rename database from postgres to dashboard, add constraints to db

--- src/main/kotlin/com/milosgarunovic/dashboard/domain/Task.kt
+++ src/main/kotlin/com/milosgarunovic/dashboard/domain/Task.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 data class Task(
     val id: String = UUID.randomUUID().toString(),
     val name: String,
-    val description: String,
+    val description: String?,
     val completed: Boolean = false,
 //    val active: Boolean = true,
 //    val tags: List<String>?,
