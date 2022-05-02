0e3bb86a4d2fcc2a7b342fed4b4fbf4ebfe5adc8
milos.garunovic <milos.garunovic@gmail.com>
2022-05-02 18:11:27 +0200

    added logback and updated controllers

--- src/main/kotlin/com/milosgarunovic/dashboard/controller/TaskController.kt
+++ src/main/kotlin/com/milosgarunovic/dashboard/controller/TaskController.kt
@@ -2,33 +2,41 @@ package com.milosgarunovic.dashboard.controller
 
 import com.milosgarunovic.dashboard.domain.Task
 import com.milosgarunovic.dashboard.repository.TaskRepositoryImpl
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/task")
 class TaskController(val taskRepositoryImpl: TaskRepositoryImpl) {
 
-    @GetMapping
+    @GetMapping(produces = ["application/json"])
     fun getAll(): List<Task> {
         return taskRepositoryImpl.getAll()
     }
 
-    @GetMapping("/{id}")
-    fun getById(@PathVariable id: String): Task? {
-        return taskRepositoryImpl.getById(id)
+    @GetMapping("/{id}", produces = ["application/json"])
+    fun getById(@PathVariable id: String): ResponseEntity<Task> {
+        val task = taskRepositoryImpl.getById(id)
+        if (task != null) {
+            return ResponseEntity(task, HttpStatus.OK)
+        }
+        return ResponseEntity(null, HttpStatus.NOT_FOUND)
     }
 
-    @PostMapping
+    @PostMapping(consumes = ["application/json"], produces = ["application/json"])
+    @ResponseStatus(HttpStatus.CREATED)
     fun add(@RequestBody task: Task): Task {
         return taskRepositoryImpl.add(task)
     }
 
-    @PostMapping("/update")
+    @PostMapping("/update", consumes = ["application/json"], produces = ["application/json"])
     fun update(@RequestBody task: Task): Task {
         return taskRepositoryImpl.update(task)
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}", produces = ["application/json"])
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun delete(@PathVariable id: String) {
         taskRepositoryImpl.delete(id)
     }
--- src/main/kotlin/com/milosgarunovic/dashboard/controller/UserRegistrationController.kt
+++ src/main/kotlin/com/milosgarunovic/dashboard/controller/UserRegistrationController.kt
@@ -2,10 +2,12 @@ package com.milosgarunovic.dashboard.controller
 
 import com.milosgarunovic.dashboard.domain.User
 import com.milosgarunovic.dashboard.repository.UserRepositoryImpl
+import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,7 +17,8 @@ class UserRegistrationController(
     val passwordEncoder: PasswordEncoder,
 ) {
 
-    @PostMapping
+    @PostMapping(consumes = ["application/json"], produces = ["application/json"])
+    @ResponseStatus(HttpStatus.CREATED)
     fun register(@RequestBody user: User) {
         val userCopy = user.copy(password = passwordEncoder.encode(user.password))
         userRepositoryImpl.add(userCopy)
