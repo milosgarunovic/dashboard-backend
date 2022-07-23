4ad43df9b7c5187d6290b14a150297f6e0bb2c98
milos.garunovic <milos.garunovic@gmail.com>
2022-07-23 22:53:52 +0200

    added SecurityFilterChainPrinter that runs on startup

--- src/main/kotlin/com/milosgarunovic/dashboard/DashboardBackendApplication.kt
+++ src/main/kotlin/com/milosgarunovic/dashboard/DashboardBackendApplication.kt
@@ -1,11 +1,33 @@
 package com.milosgarunovic.dashboard
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.web.FilterChainProxy
+import org.springframework.stereotype.Component
+import javax.servlet.Filter
+
 
 @SpringBootApplication
 class DashboardBackendApplication
 
 fun main(args: Array<String>) {
     runApplication<DashboardBackendApplication>(*args)
+}
+
+@Component
+class SecurityFilterChainPrinter : CommandLineRunner {
+
+    @Autowired
+    @Qualifier("springSecurityFilterChain")
+    private lateinit var springSecurityFilterChain: Filter
+
+    override fun run(vararg args: String?) {
+        val filterChainProxy = springSecurityFilterChain as FilterChainProxy
+        filterChainProxy.filterChains
+            .flatMap { it.filters }
+            .forEachIndexed { i, filter -> println("$i ${filter.javaClass}") }
+    }
 }
