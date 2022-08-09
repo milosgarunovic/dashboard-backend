f87e44311abea65fffc6216391f42c512ac0cab3
milos.garunovic <milos.garunovic@gmail.com>
2022-08-09 12:36:23 +0200

    added more registration tests

--- src/main/kotlin/com/milosgarunovic/dashboard/api/RegisterRequest.kt
+++ src/main/kotlin/com/milosgarunovic/dashboard/api/RegisterRequest.kt
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 class RegisterRequest(
-    @field:Email
+    @field:Email(message = "email field must be a well-formed email address")
     @field:NotNull
     val email: String?,
 
--- src/test/kotlin/com/milosgarunovic/dashboard/RegistrationTests.kt
+++ src/test/kotlin/com/milosgarunovic/dashboard/RegistrationTests.kt
@@ -22,7 +22,7 @@ class RegistrationTests : AbstractTestContainers() {
     private lateinit var objectMapper: ObjectMapper
 
     @Test
-    fun `register user returns 201 created`() {
+    fun `register user returns 201 Created`() {
         val content = objectMapper.writeValueAsBytes(mapOf("email" to "email1@gmail.com", "password" to "password"))
         mockMvc.perform(
             post("/user/register")
@@ -32,7 +32,7 @@ class RegistrationTests : AbstractTestContainers() {
     }
 
     @Test
-    fun `register same email twice returns error`() {
+    fun `register same email twice returns 209 Conflict`() {
         val content = objectMapper.writeValueAsBytes(mapOf("email" to "sameEmail@gmail.com", "password" to "password"))
         // first one passes since it doesn't exist
         mockMvc.perform(
@@ -47,5 +47,70 @@ class RegistrationTests : AbstractTestContainers() {
                 .content(content)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isConflict)
+    }
+
+    @Test
+    fun `register with no body returns 400 Bad Request`() {
+        mockMvc.perform(post("/user/register").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `register with no body and no Content-Type header returns 415 Unsupported Media Type`() {
+        mockMvc.perform(post("/user/register")).andExpect(status().isUnsupportedMediaType)
+    }
+
+    @Test
+    fun `register with wrong email format returns 400 Bad Request`() {
+        val content = objectMapper.writeValueAsBytes(mapOf("email" to "notEmail", "password" to "password"))
+        mockMvc.perform(
+            post("/user/register")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest)
+        // TODO expect to have a message in body explaining why it's a bad request
+    }
+
+    @Test
+    fun `register with empty fields returns 400 Bad Request`() {
+        val content = objectMapper.writeValueAsBytes(mapOf("email" to null, "password" to null))
+        mockMvc.perform(
+            post("/user/register")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest)
+        // TODO expect to have a message in body explaining why it's a bad request
+    }
+
+    @Test
+    fun `register with empty email returns 400 Bad Request`() {
+        val content = objectMapper.writeValueAsBytes(mapOf("email" to null, "password" to "password"))
+        mockMvc.perform(
+            post("/user/register")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest)
+        // TODO expect to have a message in body explaining why it's a bad request
+    }
+
+    @Test
+    fun `register with empty password returns 400 Bad Request`() {
+        val content = objectMapper.writeValueAsBytes(mapOf("email" to "emptyPassword@gmail.com", "password" to null))
+        mockMvc.perform(
+            post("/user/register")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest)
+        // TODO expect to have a message in body explaining why it's a bad request
+    }
+
+    @Test
+    fun `register with password with less than 8 chars returns 400 Bad Request`() {
+        val content = objectMapper.writeValueAsBytes(mapOf("email" to "goodEmail@gmail.com", "password" to "pass"))
+        mockMvc.perform(
+            post("/user/register")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest)
+        // TODO expect to have a message in body explaining why it's a bad request
     }
 }
