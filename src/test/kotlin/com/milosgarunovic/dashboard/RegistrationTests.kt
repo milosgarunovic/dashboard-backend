89e1bc9468e31e6cdc0dd86a0dc19b9dc44e27e5
milos.garunovic <milos.garunovic@gmail.com>
2022-08-10 00:33:58 +0200

    minor updates (not code related)

--- src/test/kotlin/com/milosgarunovic/dashboard/RegistrationTests.kt
+++ src/test/kotlin/com/milosgarunovic/dashboard/RegistrationTests.kt
@@ -1,7 +1,6 @@
 package com.milosgarunovic.dashboard
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
