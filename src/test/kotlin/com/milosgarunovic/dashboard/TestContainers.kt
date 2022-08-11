c049eb7fea3d1229649eb6f9487d0363132bc849
milos.garunovic <milos.garunovic@gmail.com>
2022-08-11 23:16:03 +0200

    added tmpfs to testcontainers

--- src/test/kotlin/com/milosgarunovic/dashboard/TestContainers.kt
+++ src/test/kotlin/com/milosgarunovic/dashboard/TestContainers.kt
@@ -2,6 +2,7 @@ package com.milosgarunovic.dashboard
 
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.wait.strategy.Wait
+import java.util.Collections.singletonMap
 
 object TestContainers {
 
@@ -10,6 +11,7 @@ object TestContainers {
     // TODO dockerImageName should be the same as in docker-compose, figure out how to define for both in one place
     private fun startContainer() = PostgreSQLContainer<Nothing>("postgres:14.2-alpine3.15").apply {
         setWaitStrategy(Wait.forListeningPort())
+        withTmpFs(singletonMap("/var/lib/postgresql/data", "rw"))
         start()
     }
 }
